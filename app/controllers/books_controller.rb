556c9804b1a97935a33f0b4b841f8aa5eb4fcedf
RyutoMotohashi <95869388+ryutomotohashi@users.noreply.github.com>
2022-01-27 14:10:42 +0000

    [add]4-課題

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,17 +1,18 @@
 class BooksController < ApplicationController
   def index
-    @book = Book.new
+    @book_new = Book.new
     @books = Book.all
   end
 
   def show
     @book = Book.find(params[:id])
+    @book_new = Book.new
   end
 
   def create
-    @book = Book.new(book_params)
-    @book.user_id = current_user.id
-    @book.save
+    @book_new = Book.new(book_params)
+    @book_new.user_id = current_user.id
+    @book_new.save
     redirect_to books_path
   end
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,12 +2,13 @@ class UsersController < ApplicationController
   def index
     @users = User.all
     @user = current_user
-    
+    @book_new = Book.new
   end
 
   def show
     @user = User.find(params[:id])
     @books = @user.books
+    @book_new = Book.new
   end
 
   def edit
