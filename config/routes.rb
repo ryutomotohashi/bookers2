80e924e40b80299d2e5cabcd934f4beea01d12c7
RyutoMotohashi <95869388+ryutomotohashi@users.noreply.github.com>
2022-01-29 12:35:05 +0000

    [add]4-課題

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
 
   devise_for :users
-  root to: "home#top"
-  get "home/about" => "home#about", as: "about"
+  root to: "homes#top"
+  get "home/about" => "homes#about", as: "about"
 
   resources :users, only:[:index,:show,:edit,:update]
   resources :books, only:[:index,:show,:edit,:create,:update,:destroy]
